67c5caf809c254f18fd700b3e510a539ff6bcf25
Jayminho <jayme.campos17@gmail.com>
2025-02-22 23:34:32 -0300

    criação e estruturação do footer e estilização

--- src/componentes/Rodape/index.jsx
+++ src/componentes/Rodape/index.jsx
@@ -1,9 +1,30 @@
+import styles from './Rodape.module.css';
+import Image from 'next/image';
+import logoSite from '../../../public/logo.png';
+
 export default function Rodape() {
     return (
-        <>
-            <div>
-                Footer!
+        <footer className={styles.footer}>
+            <div className={styles.footer_container}>
+                <Image src={logoSite}/>
+                <div className={styles.footer_card}>
+                    <h3>Endereço</h3>
+                    <p>Av. Bernardino de Campos, 98
+                    <br/>    
+                    São Paulo, SP 12345-678</p>
+                </div>           
+                <div className={styles.footer_card}>
+                    <h3>Contato</h3>
+                    <p>info@meusite.com 
+                    <br/>
+                    Tel: (11) 3456-7890 </p>
+                </div>           
+                <div className={styles.footer_card}>
+                    <h3>Horários</h3>
+                    <p>ABERTO TODOS OS DIAS 10:00 - 22:00</p>
+                </div>           
             </div>
-        </>
+            <p className={styles.footer_dev}>Gelateria. Orgulhosamente desenvolvido por Jayme Campos</p>
+        </footer>
     )
 }
